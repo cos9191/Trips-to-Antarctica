7c6235ad1f8ce52f73c0fe3b46d5efc23bf3b118
Konstantin Fedorov <cos9191@mail.ru>
2023-04-19 15:38:43 +0700

    completed header&footer

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@ import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {CustomSelect} from './modules/select/custom-select';
 import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
+import { Burger } from './modules/header/burger';
 
 // ---------------------------------
 
@@ -15,6 +16,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modules
   // ---------------------------------
+
+  const burger = new Burger();
+  burger.init();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
@@ -34,16 +38,16 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
 
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
+// привязывайте js не на классы, а на дата-атрибуты (data-validate)
 
 // вместо модификаторов .block--active используем утилитарные классы
 // .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
 // .select.select--opened ❌ ---> [data-select].is-open ✅
 
-// выносим все в дата атрибуты
+// выносим все в дата-атрибуты
 // url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
 
-// для адаптивного JS используейтся matchMedia и addListener
+// для адаптивного JS используется matchMedia и addListener
 // const breakpoint = window.matchMedia(`(min-width:1024px)`);
 // const breakpointChecker = () => {
 //   if (breakpoint.matches) {
