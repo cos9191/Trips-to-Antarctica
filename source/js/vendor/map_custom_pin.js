c1640b8747789c1f035c140387aac3c9a10e9037
Konstantin Fedorov <cos9191@mail.ru>
2023-04-26 18:20:40 +0700

    map perfomance fix

--- source/js/vendor/map_custom_pin.js
+++ source/js/vendor/map_custom_pin.js
@@ -12,30 +12,90 @@ const mapPin = {
   zoom: 16,
 }
 
-ymaps.ready(function () {
-    let myMap = new ymaps.Map('map', {
-            center: [mapPin.position.x, mapPin.position.y],
-            zoom: mapPin.zoom,
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
+const isMobile = {
+  android() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  blackBerry() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  opera() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  windows() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any() {
+    return (isMobile.android() || isMobile.blackBerry() || isMobile.iOS() || isMobile.opera() || isMobile.windows());
+  },
+};
 
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Собственный значок метки',
-            balloonContent: 'г. Санкт Петербург, ул. Большая Конюшенная, 19/8\''
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: 'img/svg/map_pin.svg',
-            // Размеры метки.
-            iconImageSize: [mapPin.size.width, mapPin.size.height],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-mapPin.size.width/2, -mapPin.size.height]
-        });
+const initMap = () => {
+  let myMap = new ymaps.Map('map', {
+      center: [mapPin.position.x, mapPin.position.y],
+      zoom: mapPin.zoom,
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
 
-    myMap.geoObjects
-        .add(myPlacemark);
-})
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'Собственный значок метки',
+      balloonContent: 'г. Санкт Петербург, ул. Большая Конюшенная, 19/8\''
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/svg/map_pin.svg',
+      // Размеры метки.
+      iconImageSize: [mapPin.size.width, mapPin.size.height],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-mapPin.size.width/2, -mapPin.size.height]
+    });
+  myMap.geoObjects.add(myPlacemark);
+
+  if (isMobile.any()) {
+    myMap.behaviors.disable('drag');
+  }
+}
+// ymaps.ready(function () {
+//     let myMap = new ymaps.Map('map', {
+//             center: [mapPin.position.x, mapPin.position.y],
+//             zoom: mapPin.zoom,
+//         }, {
+//             searchControlProvider: 'yandex#search'
+//         }),
+//
+//         myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+//             hintContent: 'Собственный значок метки',
+//             balloonContent: 'г. Санкт Петербург, ул. Большая Конюшенная, 19/8\''
+//         }, {
+//             // Опции.
+//             // Необходимо указать данный тип макета.
+//             iconLayout: 'default#image',
+//             // Своё изображение иконки метки.
+//             iconImageHref: 'img/svg/map_pin.svg',
+//             // Размеры метки.
+//             iconImageSize: [mapPin.size.width, mapPin.size.height],
+//             // Смещение левого верхнего угла иконки относительно
+//             // её "ножки" (точки привязки).
+//             iconImageOffset: [-mapPin.size.width/2, -mapPin.size.height]
+//         });
+//     myMap.geoObjects.add(myPlacemark);
+//
+//   if (isMobile.any()) {
+//     myMap.behaviors.disable('drag');
+//   }
+// })
+
+setTimeout(() => {
+  const elem = document.createElement('script');
+  elem.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=7c7d3ac5-a476-47ae-a0ab-fd6f1a9a9daa';
+  elem.type = 'text/javascript';
+  document.querySelector('body').appendChild(elem);
+  elem.onload = () => ymaps.ready(initMap);
+}, 3000)
