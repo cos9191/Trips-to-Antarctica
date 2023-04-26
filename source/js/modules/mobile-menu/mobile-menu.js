8f144ec2e8fc8ff4fc5b6bcd426d34829bc6d166
Konstantin Fedorov <cos9191@mail.ru>
2023-04-27 04:09:21 +0700

    header's logo fixes

--- source/js/modules/mobile-menu/mobile-menu.js
+++ source/js/modules/mobile-menu/mobile-menu.js
@@ -1,9 +1,14 @@
 import {ScrollLock} from './../../utils/scroll-lock.js';
 import {FocusLock} from './../../utils/focus-lock';
 
+const logoColors = {
+  white: '#f9fbfd',
+  black: '#011c40',
+};
+
 export class MobileMenu {
   constructor() {
-    this._header = document.querySelector('[data-mobile-menu]');
+    this._header = document.querySelector('[data-header]');
     this._headerContainer = document.querySelector('[data-mobile-menu-container]');
     this._mobileMenu = document.querySelector('[data-mobile-menu]');
     this._burger = document.querySelector('[data-burger]');
@@ -16,6 +21,7 @@ export class MobileMenu {
     this._onDocumentKeydown = this._onDocumentKeydown.bind(this);
     this._onDocumentClick = this._onDocumentClick.bind(this);
     this._onLinkClick = this._onLinkClick.bind(this);
+    this._onWindowResize = this._onWindowResize.bind(this);
   }
 
   init() {
@@ -23,7 +29,9 @@ export class MobileMenu {
       return;
     }
 
+    this._headerContainer.insertBefore(this._logo, this._headerContainer.firstChild);
     this._burger.addEventListener('click', this._onBurgerClick);
+    this._onWindowResize();
   }
 
   _openMenu() {
@@ -31,6 +39,8 @@ export class MobileMenu {
     this._header.classList.add('is-open');
     this._scrollLock.disableScrolling();
     this._mobileMenu.insertBefore(this._logo, this._mobileMenu.firstChild);
+    this._logo.style.color = logoColors.black;
+
     document.addEventListener('keydown', this._onDocumentKeydown);
     document.addEventListener('click', this._onDocumentClick);
     document.addEventListener('click', this._onLinkClick);
@@ -45,6 +55,8 @@ export class MobileMenu {
     this._header.classList.remove('is-open');
     this._scrollLock.enableScrolling();
     this._focusLock.unlock('[data-mobile-menu]');
+    this._logo.style.color = logoColors.white;
+
     this._headerContainer.insertBefore(this._logo, this._headerContainer.firstChild);
     document.removeEventListener('keydown', this._onDocumentKeydown);
     document.removeEventListener('click', this._onDocumentClick);
@@ -78,5 +90,23 @@ export class MobileMenu {
     if (evt.target.hasAttribute('href')) {
       this._closeMenu();
     }
+  }
+
+  _onWindowResize() {
+    const breakpointExpansion = window.matchMedia('(min-width:767px)');
+    const breakpointShrinkage = window.matchMedia('(max-width:767px)');
+
+    const breakpointChecker = () => {
+      if (breakpointExpansion.matches) {
+        this._logo.style.color = logoColors.white;
+        this._mobileMenu.insertBefore(this._logo, this._mobileMenu.firstChild);
+      }
+      if (breakpointShrinkage.matches) {
+        this._closeMenu();
+      }
+    };
+
+    breakpointExpansion.addEventListener('change', breakpointChecker);
+    breakpointChecker();
   }
 }
--- source/js/vendor/map_custom_pin.js
+++ source/js/vendor/map_custom_pin.js
@@ -62,35 +62,6 @@ const initMap = () => {
     myMap.behaviors.disable('drag');
   }
 }
-// ymaps.ready(function () {
-//     let myMap = new ymaps.Map('map', {
-//             center: [mapPin.position.x, mapPin.position.y],
-//             zoom: mapPin.zoom,
-//         }, {
-//             searchControlProvider: 'yandex#search'
-//         }),
-//
-//         myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-//             hintContent: 'Собственный значок метки',
-//             balloonContent: 'г. Санкт Петербург, ул. Большая Конюшенная, 19/8\''
-//         }, {
-//             // Опции.
-//             // Необходимо указать данный тип макета.
-//             iconLayout: 'default#image',
-//             // Своё изображение иконки метки.
-//             iconImageHref: 'img/svg/map_pin.svg',
-//             // Размеры метки.
-//             iconImageSize: [mapPin.size.width, mapPin.size.height],
-//             // Смещение левого верхнего угла иконки относительно
-//             // её "ножки" (точки привязки).
-//             iconImageOffset: [-mapPin.size.width/2, -mapPin.size.height]
-//         });
-//     myMap.geoObjects.add(myPlacemark);
-//
-//   if (isMobile.any()) {
-//     myMap.behaviors.disable('drag');
-//   }
-// })
 
 setTimeout(() => {
   const elem = document.createElement('script');
